d7a8ae9f7b679c7258ba54fa68e4cdf48b9f68f1
ChrisH0125 <ho.chris.cs@gmail.com>
2025-08-25 18:29:31 -0400

    Added credentials and authentication git push

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
 from tweepy import Tkinter
+
+consumer_key = 'consumer key'
+consumer_secret = 'consumer secrets'
+access_token = 'access token'
+access_token_secret = 'access tokens secret'
+
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+api = tweepy.API(auth)
